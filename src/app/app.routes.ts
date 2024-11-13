0d3a676d98e73fe558f238ebad52943cbeb31eaa
Kevin Michel <kevin.rabefaritra@gmail.com>
2024-11-13 11:34:19 +0100

    Added Privacy Policy link to user menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,7 @@ import { PrivacyComponent } from './shared/privacy/privacy.component';
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: PostListComponent },
-  { path: 'privacy', component: PrivacyComponent },
+  { path: 'privacy', component: PrivacyComponent, title: 'privacy policy' },
   { path: 'signout', component: SignOutComponent },
   { path: 'search', component: PostListComponent },
   { path: 'auth', component: AuthComponent },
